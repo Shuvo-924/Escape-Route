1862f3a3b6efd45bafda4f7ba48e334457456ae3
Shuvo-924 <darksk22@gmail.com>
2025-08-02 22:14:27 +0600

    Update Game.cpp

--- Game.cpp
+++ Game.cpp
@@ -1,15 +1,27 @@
 #include "Game.h"
 
 //Private functions
+void Game::reset()
+{
+	if (currentState == PLAYING) {
+		currentState = RESTARTING_FADE_OUT;
+		fadeClock.restart();
+	}
+}
+
 void Game::initVar()
 {
+	currentState = PROMPT;
 	this->window = nullptr;
-	this->playerCar.loadFromFile("Car(P).png");
+	this->disp.loadFromFile("Gravity.ttf");
+	this->start = -1;
+	this->end = -1;
 	this->bg.loadFromFile("Routes_Map1.png");
 	this->rd.setTexture(bg);
-	this->rd.setScale(0.1171875, 0.1171620742);
+	this->rd.setScale(0.1171875, 0.1171875);
+	this->playerCar.loadFromFile("Car(P).png");
 	this->plr.setTexture(playerCar);
-	this->plr.setScale(0.0045,0.0045);
+	this->plr.setScale(0.04,0.04);
 	ar.loadFromFile("Arrow.png");
 	ifstream rasta("path.txt");
 	float x, y;
@@ -29,108 +41,23 @@ void Game::initVar()
 	start.close();
 	const string graphCacheFile = "roadGraph.cache";
 	loadRoadGraph(graphCacheFile);
-	isAnimatingPath = false;      // We are not animating initially.
-	pathAnimationIndex = 0;       // Start at the beginning of the path.
-	pathCycleI = 0;               // Start the cycle at node 0...
-	pathCycleJ = 1;               // ...to node 1.
-	pathCycleTimer.restart();
-	/*this->plr.setPosition(0, 398);
+	isAnimatingPath = false;
+	pathAnimationIndex = 0;
+	this->plr.setPosition(300, 398);
+	view.setCenter(plr.getPosition());
 	this->plr.setRotation(0);
 	FloatRect bounds = this->plr.getGlobalBounds();
-	this->plr.setOrigin(this->plr.getPosition().x + bounds.width / 2, this->plr.getPosition().y + bounds.height / 2);*/
+	this->plr.setOrigin(this->plr.getPosition().x + bounds.width / 2, this->plr.getPosition().y + bounds.height / 2);
 }
 
 void Game::initWin()
 {
 	this->desktopMode = VideoMode::getDesktopMode();
 	this->window = new RenderWindow(this->desktopMode, "Escape Route", Style::Close | Style::Titlebar);
+	this->map = new TileMap("map_tiles", TILE_SIZE, MAP_WIDTH_TILES, MAP_HEIGHT_TILES);
+	fadeShape.setSize(Vector2f(this->desktopMode.width, this->desktopMode.height));
+	fadeShape.setFillColor(Color(0, 0, 0, 0));
 }
-
-//void Game::buildComplexRoadGraph()
-//{
-//	roadGraph.clear();
-//	const int searchRadius = 2;
-//	for (const auto& coord : bluePath) {
-//		for (int dy = -searchRadius; dy <= searchRadius; ++dy) {
-//			for (int dx = -searchRadius; dx <= searchRadius; ++dx) {
-//				if (dx == 0 && dy == 0) continue;
-//				Vector2f neighbor = coord + Vector2f(dx, dy);
-//				if (bluePath.count(neighbor)) {
-//					roadGraph[coord].push_back(neighbor);
-//				}
-//			}
-//		}
-//	}
-//
-//	// Connect all green path segments
-//	for (const auto& coord : greenPath) {
-//		for (int dy = -searchRadius; dy <= searchRadius; ++dy) {
-//			for (int dx = -searchRadius; dx <= searchRadius; ++dx) {
-//				if (dx == 0 && dy == 0) continue;
-//				Vector2f neighbor = coord + Vector2f(dx, dy);
-//				if (greenPath.count(neighbor)) {
-//					roadGraph[coord].push_back(neighbor);
-//				}
-//			}
-//		}
-//	}
-//	addBridgeEdges();
-//}
-//
-//void Game::addBridgeEdges()
-//{
-//	const int maxBridgeLength = 40;
-//	const vector<Vector2f> checkDirections = { {0, -1}, {0, 1}, {-1, 0}, {1, 0} };
-//
-//	for (const auto& coord : bluePath) {
-//		// For each blue pixel, check if it's next to a green pixel
-//		for (const auto& dir : checkDirections) {
-//			Vector2f adjacent = coord + dir;
-//
-//			if (greenPath.count(adjacent)) {
-//				// This blue pixel is a "bridge abutment".
-//				// Now we need to scan across the green path.
-//				// The scan direction is the same as the direction to the first green pixel.
-//				Vector2f currentPos = coord;
-//				for (int i = 0; i < maxBridgeLength; ++i) {
-//					currentPos += dir; // Move one step across the bridge
-//
-//					// If we find another blue pixel, we've found the other side!
-//					if (bluePath.count(currentPos)) {
-//						Vector2f otherSide = currentPos;
-//						// Add a mutual edge and stop this scan
-//						roadGraph[coord].push_back(otherSide);
-//						roadGraph[otherSide].push_back(coord);
-//						break;
-//					}
-//
-//					// If we hit a non-green pixel before finding a blue one, it's not a bridge
-//					if (!greenPath.count(currentPos)) {
-//						break;
-//					}
-//				}
-//			}
-//		}
-//	}
-//}
-//
-//void Game::saveRoadGraph(const string& filename)
-//{
-//	ofstream outFile(filename);
-//	cout << "Saving road graph to cache file: " << filename << "..." << endl;
-//
-//	// The file format will be:
-//	// NodeX NodeY : Neighbor1X Neighbor1Y Neighbor2X Neighbor2Y ...
-//	for (const auto& [node, neighbors] : roadGraph) {
-//		outFile << node.x << " " << node.y << " :";
-//		for (const auto& neighbor : neighbors) {
-//			outFile << " " << neighbor.x << " " << neighbor.y;
-//		}
-//		outFile << "\n";
-//	}
-//	outFile.close();
-//	cout << "Graph saved successfully." << endl;
-//}
 
 bool Game::loadRoadGraph(const string& filename)
 {
@@ -176,6 +103,7 @@ Game::Game() {
 
 Game::~Game() {
 	delete this->window;
+	delete this->map;
 }
 
 const bool Game::getWinOpen() const
@@ -195,88 +123,60 @@ void Game::pollEvents()
 		case Event::KeyPressed:
 			if (this->ev.key.code == Keyboard::Escape)
 				this->window->close();
-			break;
-		/*case Event::MouseWheelScrolled:
-			if (ev.mouseWheelScroll.delta > 0 && zoom > 0) {
-				this->view.zoom(0.9f);
-				this->zoom *= 0.9f;
+			if (currentState == PROMPT && ev.key.code == Keyboard::Enter) {
+				if (start == -1 && !myTextString.empty()) {
+					start = stoi(myTextString);
+					myTextString.clear();
+				}
+				else if (end == -1 && !myTextString.empty()) {
+					end = stoi(myTextString);
+					myTextString.clear();
+					if (start != -1 && end != -1) {
+						findPath(start, end);
+					}
+				}
 			}
-			else if (ev.mouseWheelScroll.delta < 0 && zoom < 2) {
-				this->view.zoom(1.1f);
-				this->zoom *= 1.1f;
-			}
-			this->window->setView(view);
-			break;
-		case Event::MouseButtonPressed:
-			if (ev.mouseButton.button == sf::Mouse::Left) {
-				this->dragging = true;
-				this->mousePrevPos = sf::Mouse::getPosition(*this->window);
+			if (this->ev.key.code == Keyboard::R && currentState != PROMPT) {
+				reset();
 			}
 			break;
+		case Event::KeyReleased:
+			rt = 0;
+			break;
+		case Event::TextEntered:
+			if (currentState == PROMPT) {
+				Uint32 unicodeChar = ev.text.unicode;
 
-		case Event::MouseButtonReleased:
-			if (ev.mouseButton.button == sf::Mouse::Left) {
-				this->dragging = false;
+				if (unicodeChar >= 32 && unicodeChar <= 126 && myTextString.size() < 2)
+				{
+					if(stoi(myTextString + static_cast<char>(unicodeChar)) < 21)
+					   myTextString += static_cast<char>(unicodeChar);
+				}
+				else if (unicodeChar == 8)
+				{
+					if (!myTextString.empty())
+					{
+						myTextString.erase(myTextString.size() - 1);
+					}
+				}
 			}
-			break;*/
 		}
 	}
 }
 
-
 void Game::update()
 {
 	this->pollEvents();
-	/*if (dragging) {
-		sf::Vector2i mouseCurrPos = sf::Mouse::getPosition(*this->window);
-		sf::Vector2f worldPrev = this->window->mapPixelToCoords(mousePrevPos);
-		sf::Vector2f worldCurr = this->window->mapPixelToCoords(mouseCurrPos);
-
-		sf::Vector2f delta = worldPrev - worldCurr;
-		this->view.move(delta);
-		this->window->setView(this->view);
-
-		this->mousePrevPos = mouseCurrPos;
-	}*/
-	/*if(cur < path.size()) {
-		plr.setPosition(path[cur]);
-		plr.setRotation(rotation[cur]);
-		cur++;
-	}*/
-	float dx, dy, ang;
-	const double pi = 3.14159265358979323846;
-	ang = this->plr.getRotation();
-	dx = speed * cosf(ang * pi / 180.f);
-	dy = speed * sinf(ang * pi / 180.f);
-	plr.move(dx, dy);
-	if (Keyboard::isKeyPressed(Keyboard::Down)) {
-		(this->speed > 0) ? this->speed -= 0.001 : this->speed = 0;
-	}
-	if (Keyboard::isKeyPressed(Keyboard::Up)) {
-		this->speed = 0.1;
-	}
-	if (Keyboard::isKeyPressed(Keyboard::Right)) {
-		plr.rotate(0.0862);
-	}
-	if (Keyboard::isKeyPressed(Keyboard::Left)) {
-		plr.rotate(-0.0862);
-	}
-	
-	view.setCenter(plr.getPosition());//Setting the view around the moving Car
-	view.setSize(320, 180);
-	//A red dot marking the position of the Player in the MiniMap
-	mark.setPosition(plr.getPosition().x + (plr.getGlobalBounds().width / 2) * sinf(plr.getRotation() * pi / 180.f), plr.getPosition().y - (plr.getGlobalBounds().height / 2) * cosf(plr.getRotation() * pi / 180.f));
-	mark.setRotation(plr.getRotation());
 	if (isAnimatingPath) {
 		if (pathAnimTimer.getElapsedTime() > pathAnimInterval && pathAnimationIndex < shortestPath.size()) {
-			if (pathAnimationIndex < shortestPath.size() - 12) {
+			if (pathAnimationIndex < shortestPath.size() - 15) {
 				Sprite newArrow;
-				newArrow.setTexture(ar); 
-				newArrow.setScale(0.03, 0.03); 
-				newArrow.setOrigin(ar.getSize().x / 2.f, ar.getSize().y / 2.f); 
+				newArrow.setTexture(ar);
+				newArrow.setScale(0.03, 0.03);
+				newArrow.setOrigin(ar.getSize().x / 2.f, ar.getSize().y / 2.f);
 
 				const Vector2f& currentPoint = shortestPath[pathAnimationIndex];
-				const Vector2f& nextPoint = shortestPath[pathAnimationIndex + 12];
+				const Vector2f& nextPoint = shortestPath[pathAnimationIndex + 15];
 
 				newArrow.setPosition(currentPoint);
 				Vector2f direction = nextPoint - currentPoint;
@@ -284,39 +184,169 @@ void Game::update()
 				newArrow.setRotation(angle);
 
 				pathArrowSprites.push_back(newArrow);
+				plr.setPosition(pathArrowSprites[0].getPosition().x * RENDER_WORLD_SCALE_X, pathArrowSprites[0].getPosition().y * RENDER_WORLD_SCALE_Y);
+				plr.setRotation(pathArrowSprites[0].getRotation());
+				plr.setPosition(plr.getPosition().x - plr.getGlobalBounds().width * 10 * cosf(plr.getRotation() * pi / 180.f), plr.getPosition().y - plr.getGlobalBounds().width * 10 * sinf(plr.getRotation() * pi / 180.f));
+				speed = 1.f;
 			}
-			pathAnimationIndex+=12;
+			pathAnimationIndex += 15;
 			pathAnimTimer.restart();
 		}
 
 		if (pathAnimationIndex >= shortestPath.size()) {
 			isAnimatingPath = false;
-			pathCycleTimer.restart();
+			currentState = FADING_OUT;
+			fadeClock.restart();
+		}
+	}
+
+	switch (currentState)
+	{
+	case PROMPT:
+		break;
+
+	case FADING_OUT:
+	{
+		// Calculate progress (0.0 to 1.0)
+		float progress = fadeClock.getElapsedTime().asSeconds() / fadeDuration.asSeconds();
+		if (progress > 1.0f) {
+			progress = 1.0f;
 		}
 
-	}
-	else {
-		if (pathCycleTimer.getElapsedTime() > pathCycleDelay) {
-			pathCycleJ++;
-			if (pathCycleJ >= endpoints.size()) {
-				pathCycleI++;
-				pathCycleJ = 0;
-			}
-			if (pathCycleI >= endpoints.size()) {
-				pathCycleI = 0;
-			}
-			if (pathCycleI == pathCycleJ) {
-				pathCycleJ++;
-				if (pathCycleJ >= endpoints.size()) {
-					pathCycleI++;
-					pathCycleJ = 0;
-				}
-			}
-			if (pathCycleI < endpoints.size()) {
-				findPath(pathCycleI, pathCycleJ);
-			}
+		// Fade from transparent (0) to opaque (255)
+		Uint8 alpha = static_cast<Uint8>(progress * 255);
+		fadeShape.setFillColor(Color(0, 0, 0, alpha));
+
+		// When fully faded out...
+		if (progress >= 1.0f) {
+			// ...instantly position the camera on the player...
+			const Vector2f mapSize(29335.0f, 16504.0f);
+			view.setSize(1920, 1080);
+			Vector2f viewSize = view.getSize();
+			Vector2f desiredCenter = plr.getPosition();
+			float minX = viewSize.x / 2.0f;
+			float maxX = mapSize.x - viewSize.x / 2.0f;
+			float minY = viewSize.y / 2.0f;
+			float maxY = mapSize.y - viewSize.y / 2.0f;
+			view.setCenter(
+				clamp(desiredCenter.x, minX, maxX),
+			    clamp(desiredCenter.y, minY, maxY)
+			);
+			currentState = FADING_IN;
+			fadeClock.restart();
 		}
+		break;
 	}
+
+	case FADING_IN:
+	{
+		float progress = fadeClock.getElapsedTime().asSeconds() / fadeDuration.asSeconds();
+		if (progress > 1.f) {
+			progress = 1.f;
+		}
+
+		Uint8 alpha = static_cast<Uint8>(255 - (progress * 255));
+		fadeShape.setFillColor(Color(0, 0, 0, alpha));
+
+		if (progress >= 1.f) {
+			currentState = PLAYING;
+		}
+		break;
+	}
+	case PLAYING:
+	{
+		float dx, dy, ang;
+		const double pi = 3.14159265358979323846;
+		ang = this->plr.getRotation();
+		dx = speed * cosf(ang * pi / 180.f);
+		dy = speed * sinf(ang * pi / 180.f);
+		plr.move(dx, dy);
+		if (Keyboard::isKeyPressed(Keyboard::Down)) {
+			(this->speed > 0) ? this->speed -= 0.008 : this->speed = 0;
+		}
+		if (Keyboard::isKeyPressed(Keyboard::Up)) {
+			if (this->speed < 2.5)
+			this->speed += 0.0025;
+			else this->speed = 2.5;
+		}
+		if (Keyboard::isKeyPressed(Keyboard::Right)) {
+			if (rt < 0.3) {
+				rt += 0.0012;
+			}
+			plr.rotate(rt);
+		}
+		if (Keyboard::isKeyPressed(Keyboard::Left)) {
+			if (rt < 0.3) {
+				rt += 0.0012;
+			}
+			plr.rotate(-rt);
+		}
+		double fact = speed;
+		const Vector2f mapSize(29335.0f, 16504.0f);
+		view.setSize(960 * (1 + fact), 540 * (1 + fact));
+		Vector2f viewSize = view.getSize();
+
+		Vector2f desiredCenter = plr.getPosition();
+
+		Vector2f finalCenter = desiredCenter;
+
+		float minX = viewSize.x / 2.0f;
+		float maxX = mapSize.x - viewSize.x / 2.0f;
+		finalCenter.x = max(minX, min(finalCenter.x, maxX));
+
+		float minY = viewSize.y / 2.0f;
+		float maxY = mapSize.y - viewSize.y / 2.0f;
+		finalCenter.y = max(minY, min(finalCenter.y, maxY));
+
+		view.setCenter(finalCenter);
+
+		//A red dot marking the position of the Player in the MiniMap
+		mark.setPosition(plr.getPosition().x + (plr.getGlobalBounds().width / 2) * sinf(plr.getRotation() * pi / 180.f), plr.getPosition().y - (plr.getGlobalBounds().height / 2) * cosf(plr.getRotation() * pi / 180.f));
+		mark.setRotation(plr.getRotation());
+		break;
+	}
+	case RESTARTING_FADE_OUT:
+	{
+		float progress = fadeClock.getElapsedTime().asSeconds() / fadeDuration.asSeconds();
+		if (progress > 1.0f) {
+			progress = 1.0f;
+		}
+
+		// Fade from transparent (0) to opaque (255)
+		Uint8 alpha = static_cast<Uint8>(progress * 255);
+		fadeShape.setFillColor(Color(0, 0, 0, alpha));
+
+		// When fully black...
+		if (progress >= 1.0f) {
+			currentState = RESTARTING_FADE_IN;
+			start = -1;
+			end = -1;
+			shortestPath.clear();
+			pathArrowSprites.clear();
+			myTextString.clear();
+			isAnimatingPath = false;
+			pathAnimationIndex = 0;
+			speed = 0.f;
+			fadeClock.restart();
+		}
+		break;
+	}
+	case RESTARTING_FADE_IN:
+	{
+		float progress = fadeClock.getElapsedTime().asSeconds() / fadeDuration.asSeconds();
+		if (progress > 1.0f) {
+			progress = 1.0f;
+		}
+
+		Uint8 alpha = static_cast<Uint8>(255 - (progress * 255));
+		fadeShape.setFillColor(Color(0, 0, 0, alpha));
+
+		if (progress >= 1.0f) {
+			currentState = PROMPT;
+		}
+		break;
+	}
+    }
 }
 
 void Game::findPath(int x, int y)
@@ -326,13 +356,13 @@ void Game::findPath(int x, int y)
 
 	this->shortestPath = findShortestPath(startNode, endNode);
 	if (!shortestPath.empty()) {
-		isAnimatingPath = true;       // Turn the animation on.
-		pathAnimationIndex = 0;       // Reset to the start of the new path.
-		pathArrowSprites.clear();     // Clear out the arrows from the previous path.
-		pathAnimTimer.restart();      // Restart the timer for placing the first arrow.
+		isAnimatingPath = true;       
+		pathAnimationIndex = 0;       
+		pathArrowSprites.clear();
+		pathAnimTimer.restart();    
 	}
 	else {
-		isAnimatingPath = false;      // If no path, ensure animation is off.
+		isAnimatingPath = false;    
 	}
 }
 
@@ -340,7 +370,7 @@ vector<Vector2f> Game::findShortestPath(const Vector2f& start, const Vector2f& e
 {
 	if (roadGraph.find(start) == roadGraph.end() || roadGraph.find(end) == roadGraph.end()) {
 		cout << "Start or end node not in graph." << endl;
-		return {}; // Return an empty path
+		return {};
 	}
 
 	queue<Vector2f> q;
@@ -386,10 +416,68 @@ vector<Vector2f> Game::findShortestPath(const Vector2f& start, const Vector2f& e
 void Game::render()
 {
 	this->window->clear(Color::Green);
-	//this->window->setView(view);
-	this->window->draw(rd);
-	for (const auto& arrowSprite : pathArrowSprites) {
-		this->window->draw(arrowSprite);
+	if (currentState == PROMPT) {
+		this->window->draw(rd);
+		Text choose, st, en;
+		choose.setString("Choose The Starting point ->");
+		choose.setFont(disp);
+		choose.setPosition(window->getSize().x / 2 - 25 * (choose.getLetterSpacing() + choose.getString().getSize()) / 2, 100);
+		choose.setOutlineColor(Color::Black);
+		choose.setOutlineThickness(1.f);
+		choose.setCharacterSize(40);
+		st.setString(to_string(start));
+		st.setFont(disp);
+		st.setPosition(700, 200);
+		st.setOutlineColor(Color::Black);
+		st.setOutlineThickness(1.f);
+		en.setString(to_string(end));
+		en.setFont(disp);
+		en.setPosition(1000, 200);
+		en.setOutlineColor(Color::Black);
+		en.setOutlineThickness(1.f);
+		this->window->draw(choose);
+		this->window->draw(st);
+		this->window->draw(en);
+		if (isAnimatingPath) {
+			for (const auto& arrowSprite : pathArrowSprites) {
+				this->window->draw(arrowSprite);
+			}
+		}
+	}
+	else if (currentState == FADING_OUT || currentState == RESTARTING_FADE_IN) {
+		this->window->draw(rd);
+		Text choose, st, en;
+		choose.setString("Choose The Starting point ->");
+		choose.setFont(disp);
+		choose.setPosition(window->getSize().x / 2 - 30 * (choose.getLetterSpacing() + choose.getString().getSize()) / 2, 100);
+		choose.setOutlineColor(Color::Black);
+		choose.setOutlineThickness(1.f);
+		choose.setCharacterSize(40);
+		st.setString(to_string(start));
+		st.setFont(disp);
+		st.setPosition(700, 200);
+		st.setOutlineColor(Color::Black);
+		st.setOutlineThickness(1.f);
+		en.setString(to_string(end));
+		en.setFont(disp);
+		en.setPosition(1000, 200);
+		en.setOutlineColor(Color::Black);
+		en.setOutlineThickness(1.f);
+		this->window->setView(this->window->getDefaultView());
+		this->window->draw(choose);
+		this->window->draw(st);
+		this->window->draw(en);
+		this->window->draw(fadeShape);
+	}
+	else if (currentState == RESTARTING_FADE_OUT || currentState == PLAYING || currentState == FADING_IN) {
+		this->window->setView(view);
+		if (this->map) {
+			this->window->draw(*map);
+		}
+		this->window->draw(this->plr);
+		if (currentState == FADING_IN || currentState == RESTARTING_FADE_OUT) {
+			this->window->draw(fadeShape);
+		}
 	}
 	this->window->display();
 }
