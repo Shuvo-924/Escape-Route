8d3d8e7769c39467728b20c6d3400a3852050dd8
Shuvo-924 <darksk22@gmail.com>
2025-08-02 22:13:58 +0600

    Update Game.h

--- Game.h
+++ Game.h
@@ -5,6 +5,7 @@
 #include<SFML/System.hpp>
 #include<SFML/Audio.hpp>
 #include<SFML/Window.hpp>
+#include<SFML/OpenGL.hpp>
 #include<vector>
 #include<set>
 #include<iostream>
@@ -15,6 +16,7 @@
 #include<queue>
 #include<unordered_map>
 #include<unordered_set>
+#include "TileMap.h"
 
 using namespace sf;
 using namespace std;
@@ -25,20 +27,33 @@ struct Vector2fHash { //Custom Hash function to use Vector2f data type in map an
 	}
 };
 
+enum GameState {
+	PROMPT,
+	FADING_OUT,
+	FADING_IN,
+	PLAYING,
+	RESTARTING_FADE_OUT,
+	RESTARTING_FADE_IN
+};
+
 class Game
 {
 
 private:
-	RenderWindow* window;//For Game Window
-	View view;//For Viewing a certain portion of the Window
-	Event ev;//For Event handling such as Keypress,Cursor,Mouse-clicks etc.
-	VideoMode desktopMode;//For Window size
+	RenderWindow* window; //For Game Window
+	View view; //For Viewing a certain portion of the Window
+	Event ev; //For Event handling such as Keypress,Cursor,Mouse-clicks etc.
+	VideoMode desktopMode; //For Window size
+	TileMap* map; //Map visible around the Player Car
+
+	//To convert screen Coordinates to High resolution Image Coordinates
+	const float RENDER_WORLD_SCALE_X = 29335.0f / 1920.0f;
+	const float RENDER_WORLD_SCALE_Y = 16504.0f / 1080.0f;
 
 	Texture playerCar,policeCar, bg, dot, ar;//Image data for Objects to use on screen
 	Sprite plc, plr, rd, mark, arw;//Objects
-	float speed = 0, rt = 0;
+	double speed = 0, rt = 0;
 	const double pi = 3.14159265358979323846;
-	float zoom = 1.f;
 
 	unordered_set<Vector2f, Vector2fHash> bluePath; //Set of (x,y) coordinates for drawing a Path (For regular roads and overpasses)
 	unordered_set<Vector2f, Vector2fHash> greenPath; // For underpasses
@@ -46,6 +61,18 @@ private:
 	unordered_set<Vector2f, Vector2fHash> visited; //To check visited nodes
 	vector<Vector2f> endpoints; //Starting position or Destination points
 	vector<Vector2f> shortestPath; //To store the Shortest Path data
+	const int TILE_SIZE = 512;
+	const int MAP_WIDTH_TILES = 58;
+	const int MAP_HEIGHT_TILES = 33;
+
+	GameState currentState;
+	RectangleShape fadeShape; //For Transitioning into and from Black screen
+	Clock fadeClock; //To keep track of time and trigger Transition 
+	const Time fadeDuration = seconds(1.25f); //Duration of fade
+
+	Font disp;
+	int start, end;
+	string myTextString;
 
 	vector<Sprite> pathArrowSprites; // Stores the visible arrow sprites.
 	Clock pathAnimTimer; // Times the interval between arrows.
@@ -53,13 +80,9 @@ private:
 	size_t pathAnimationIndex; // Tracks which point in the path we're animating next.
 	bool isAnimatingPath; // Flag to control if the animation is running.
 
-	Clock pathCycleTimer; // Times the delay between showing different paths.
-	Time pathCycleDelay = sf::seconds(1.0f); // Delay after one path finishes before the next one starts.
-	int pathCycleI; // Current start index for cycling
-	int pathCycleJ; // Current end index for cycling
-
-	void initVar();//Function to set Initial values of Variables
-	void initWin();//Function to set Initial Window
+	void reset(); //Function to restart the Game
+	void initVar(); //Function to set Initial values of Variables
+	void initWin(); //Function to set Initial Window
 	/*void buildComplexRoadGraph();
 	void addBridgeEdges();
 	void saveRoadGraph(const string& filename);*/
