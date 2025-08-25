528100de617bb01718321557c2a369759489bb56
Shuvo-924 <darksk22@gmail.com>
2025-08-25 12:03:23 +0600

    Update SignalControl.h

--- SignalControl.h
+++ SignalControl.h
@@ -12,28 +12,24 @@ struct node{
 	int val;
 };
 
-struct sign {
-	bool stop;
-	float dir;
-};
-
 class Signal {
 
 private:
-	vector<Vector2f> dir;
-	vector<vector<vector<Vector2f>>> lightPos;
-	vector<vector<Color>> clr;
 	vector<Clock> cycle;
-	node* cycleVar;
-	node* current;
+	vector<vector<Color>> clr;
+	vector<node*> cycleVar;
+	vector<node*> current; //To cycle across the 4 signals in an Intersection
+	const Time wait = seconds(15);
 	float height, width;
 	void initvar();
 public:
 	Signal();
-	virtual ~Signal();
-	vector<Vector2f> pos;
-	vector<Vector2f> points;
-	vector<vector<sign>> stop;
+	virtual ~Signal();	
+	static vector<Vector2f> dir;
+	static vector<Vector2f> pos;
+	static vector<Vector2f> points;
+	static vector<vector<bool>> stop;
+	static vector<vector<vector<Vector2f>>> lightPos;
 	void drawsignals(RenderWindow& window, double RENDER_WORLD_SCALE_X, double RENDER_WORLD_SCALE_Y);
 	void drawColors(RenderWindow& window, double RENDER_WORLD_SCALE_X, double RENDER_WORLD_SCALE_Y, float cx, float cy, Color color);
 	void signalctrl(RenderWindow& window, double RENDER_WORLD_SCALE_X, double RENDER_WORLD_SCALE_Y);
