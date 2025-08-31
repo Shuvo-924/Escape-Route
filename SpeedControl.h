b622f090575942d94f35c347e4654f0fedaf0fb8
Shuvo-924 <darksk22@gmail.com>
2025-08-31 23:02:17 +0600

    Update SpeedControl.h

--- SpeedControl.h
+++ SpeedControl.h
@@ -1,21 +1,39 @@
-#ifndef SPEEDCONTROL_H
-#define SPEEDCONTROL_H
+#ifndef SIGNALCONTROL_H
+#define SIGNALCONTROL_H
 
 #include <SFML/Graphics.hpp>
-#include <string>
-#include <vector>
-#include <algorithm>
-#include <unordered_map>
-#include "CustomFunctions.h"
-#include "SpawnCars.h"
+#include <fstream>
 
-using namespace sf;
 using namespace std;
+using namespace sf;
 
-struct Car;
+struct node{
+	node* next;
+	int val;
+};
 
-void speedctrl(Car& car, const vector<Car*>& nearbyCars); //Function to increase or decrease the Car speed
-bool ObeySignals(Car& car); //Function to check for Red signals in front of the car
-void getBorders(Sprite& plr, double& speed);
+class Signal {
+
+private:
+	vector<Clock> cycle;
+	vector<vector<Color>> clr;
+	vector<node*> cycleVar;
+	vector<node*> current; //To cycle across the 4 signals in an Intersection
+	const Time wait = seconds(15);
+	float height, width;
+	void initvar();
+public:
+	Signal();
+	virtual ~Signal();	
+	static vector<Vector2f> dir;
+	static vector<Vector2f> pos;
+	static vector<Vector2f> points;
+	static vector<vector<bool>> stop;
+	static vector<vector<vector<Vector2f>>> lightPos;
+	void drawsignals(RenderWindow& window, double RENDER_WORLD_SCALE_X, double RENDER_WORLD_SCALE_Y);
+	void drawColors(RenderWindow& window, double RENDER_WORLD_SCALE_X, double RENDER_WORLD_SCALE_Y, float cx, float cy, Color color);
+	void signalctrl(RenderWindow& window, double RENDER_WORLD_SCALE_X, double RENDER_WORLD_SCALE_Y);
+};
 
 #endif
+
